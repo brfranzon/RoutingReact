dd8373089be42a7f059c7d9e6cbe4f0ecfa90f0f
BrunoFranzon <bruno@madhat.de>
2022-01-29 17:30:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Personagens from './componentes/personagens';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Route exact path="/">
+          <h1> Winter is coming....</h1>
+          <button>
+            <Link to="/personagens">Vamos ver os personagens...</Link>
+          </button>
+        </Route>
+
+        <Route exact path="/personagens">
+          <Personagens />
+        </Route>
+
+        {/* redirecionar rota */}
+        {/* <Route exact path="/">
+          <Redirect to="/login" />
+        </Route> */}
+
+        {/* <Route exact path="/">
+          <h1 style={{ border: "2px solid red" }}>Meu início</h1> */}
+
+        {/* const history = useHistory() */}
+        {/* <button>
+            <Link to="/rota1">Vai para rota 1</Link>
+          </button> */}
+        {/* </Route> */}
+
+        {/* <Route exact path="/rota1"> */}
+        {/* conteúdo */}
+        {/* <h1 style={{ border: "2px solid green" }}>
+            Aqui eu tenho a minha rota 1.
+          </h1> */}
+        {/* </Route> */}
+
+        {/* <Route exact path="/rota2"> */}
+        {/* <h1 style={{ border: "2px solid blue" }}>
+            Aqui eu tenho a minha rota 2.
+          </h1> */}
+        {/* </Route> */}
+
+        {/* <Route exact path="/rota1/:id"> */}
+        {/* <h1 style={{ border: "2px solid blue" }}>
+            Aqui eu tenho a minha rota 1 com parametro...
+          </h1> */}
+        {/* </Route> */}
+
+        {/* corpo */}
+        {/* <h1 style={{ border: "2px solid green" }}>Meu início</h1>
+
+        <h1 style={{ border: "2px solid green" }}>
+          Aqui eu tenho um 1 pedaco de html.
+        </h1> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/componentes/personagens.js
@@ -0,0 +1,22 @@
+// funcao
+// corpo (useSate, useEffect, hooks....)
+// return
+import { useEffect, useState } from "react";
+
+function Personagens() {
+  // JS
+  // console.log('Thais....');
+  // const numeros = [1, 2, 3, 555, -56];
+  // numeros.map(el => {return 2*el});
+  // useEffect(()=>{}, []);
+  // const {x, setX} = useState();
+
+  // JSX
+  return <h1> Personais...</h1>;
+}
+
+// const Personagens = () => {
+//   return <h1>personagens</h1>;
+// };
+
+// export default Personagens;
